b9a5c7b2b502d99267372cbe215ed271babb81de
Dan Silberman <silberman@gmail.com>
2016-02-29 17:20:42 -0800

    A bunch of cleanup in backend.

--- appengine/helloworld.py
+++ appengine/helloworld.py
@@ -8,10 +8,8 @@ import time
 import webapp2
 
 from gcloud import storage
-import cloudstorage as gcs
-from googleapiclient import discovery
-from apiclient.discovery import build
-from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery # for vision API
+from oauth2client.client import GoogleCredentials # for vision API
 
 # detection types available from google vision API:
 LABEL_DETECTION = 'LABEL_DETECTION'
@@ -35,27 +33,21 @@ ACCEPTED_DETECTION_TYPES = set([LABEL_DETECTION, TEXT_DETECTION])
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        print "MainPage get() called"
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World from appengine!')
 
 class PostPictureHandler(webapp2.RequestHandler):
+    """
+    Receives POSTs of photos/label-requests from the app, returning data gathered from the google
+    vision API.
+    """
     def post(self):
-        print '*' * 50
-        print "PostPictureHandler post() called"
-        print self.request.arguments()
-        print len(self.request.arguments())
-        print '*' * 10
         for key_index, key in enumerate(self.request.arguments()):
             value = self.request.get(key)
             print key_index, key, len(value), type(value)
 
-
         photo_data = self.request.get('photo')
         detection_type = self.request.get('detection_type')
-        print '#' * 30, detection_type
-        print ACCEPTED_DETECTION_TYPES
-        print detection_type in ACCEPTED_DETECTION_TYPES
         assert detection_type in ACCEPTED_DETECTION_TYPES
 
         # Save the data to a google storage bucket
@@ -72,7 +64,6 @@ class PostPictureHandler(webapp2.RequestHandler):
         print response_obj
         self.response.write(json.dumps(response_obj))
 
-
 def _safe_filename(filename):
     """
     Generates a safe filename that is unlikely to collide with existing objects
@@ -85,49 +76,18 @@ def _safe_filename(filename):
 
 def write_photo_via_gcloud_storage(photo_data):
     client = storage.Client(project='steam-1111')
-    print "Client:", client
     base_filename = "testfilename.jpg"
     safe_filename = _safe_filename(base_filename)
     bucket_name = 'steam-1111.appspot.com'
     bucket = client.get_bucket(bucket_name)
-    print bucket
     content_type = "image/jpeg"
     blob = bucket.blob(safe_filename)
     blob.upload_from_string(photo_data, content_type=content_type)
-    # blob.path is like: /b/steam-1111.appspot.com/o/testfilename-2016-02-28-202045.jpg
-    # so for the gs: url we can just replace /b with gs:/ and /o/ with /
-    # XXX must be a better way to get this from blob
-    gs_location = blob.path.replace('/b/', 'gs://').replace('/o/', '/')
+    gs_location = gs_location_from_bucket_and_filename(bucket_name, safe_filename)
     return gs_location
 
-
-
-def write_photo_via_credentials(photo_data):
-    raise NotImplementedError("doesn't work yet")
-    credentials = GoogleCredentials.get_application_default()
-    service = discovery.build('storage', 'v1', credentials=credentials)
-    filename = 'nameOfObject'
-    req = service.objects().insert_media(bucket='somebucket', name=filename, media_body=photo_data)
-    resp = req.execute()
-    return filename
-
-
-def write_photo(photo_from_POST):
-    write_retry_params = gcs.RetryParams(backoff_factor=1.1)
-
-    bucket_name = '/steam-1111.appspot.com'
-    filename = "%s/%s_%s.jpg" % (bucket_name, 'fromreactnative', time.time())
-
-    # https://cloud.google.com/storage/docs/reference-headers#xgoogmeta
-    gcs_file = gcs.open(filename,
-                        'w',
-                        content_type='image/jpg',
-                        options={},
-                        retry_params=write_retry_params)
-    gcs_file.write(photo_from_POST)
-    gcs_file.close()
-    logging.debug("Wrote: %s" % filename)
-    return filename
+def gs_location_from_bucket_and_filename(bucket_name, filename):
+    return "gs://%s/%s" % (bucket_name, filename)
 
 def get_vision_service():
     DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
